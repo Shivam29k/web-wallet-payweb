8de2b73935913731db8252f973fe3381b6d12197
Shivam Kumar <skumarshivam50@gmail.com>
2024-07-05 11:39:16 +0530

    chore: Update redirect URLs for supported banks in AddMoneyCard component

--- apps/user-app/app/components/AddMoneyCard.tsx
+++ apps/user-app/app/components/AddMoneyCard.tsx
@@ -18,10 +18,10 @@ declare global {
 
 const SUPPORTED_BANKS = [{
     name: "HDFC Bank",
-    redirectUrl: "http://localhost:3000/fakebankpage"
+    redirectUrl: `${process.env.NEXTAUTH_URL}/fakebankpage`
 },{
     name: "ICICI Bank",
-    redirectUrl: "http://localhost:3000/fakebankpage"
+    redirectUrl: `${process.env.NEXTAUTH_URL}/fakebankpage`
 }]
 
 
